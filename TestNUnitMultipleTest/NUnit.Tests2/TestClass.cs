fd5ea2d060f59bc64b7ccb7e4c496256673bf60f
Terje Sandstrøm <terje@hermit.no>
2020-02-16 20:49:42 +0100

    updated

---
+++ TestNUnitMultipleTest/NUnit.Tests2/TestClass.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests2
+{
+    [TestFixture]
+    public class TestClass
+    {
+
+        [SetUp]
+        public void TestSetUp()
+        {
+
+        }
+
+        
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
---
+++ TestNUnitMultipleTest/NUnit.Tests8/TestClass.cs
@@ -0,0 +1,47 @@
+﻿// NUnit3 Tests,  see documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation 
+
+using MultiTesting31;
+using NUnit.Framework;
+
+namespace NUnit.Tests8
+{
+    [TestFixture]
+    public class TestClass
+    {
+
+        [Test]
+        public void TestSetUp()
+        {
+            // Add code that runs before each test method
+
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+
+        }
+      
+    }
+
+    
+
+
+    [TestFixture]
+    public class ClassTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+
+        }
+    }
+
+
+
+   
+}
